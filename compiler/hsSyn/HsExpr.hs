6be03cb39d92f0181c778f6c896e87286d8f4f04
Johan Tibell <johan.tibell@gmail.com>
2015-09-02 18:20:08 -0700

    Add missing parens

--- compiler/hsSyn/HsExpr.hs
+++ compiler/hsSyn/HsExpr.hs
@@ -200,7 +200,7 @@ data HsExpr id
   | HsSum Int    -- Alternative (0-based)
           Arity  -- Sum arity
           (LHsExpr id)
-          PostTc id [Type]   -- the type arguments
+          (PostTc id [Type])   -- the type arguments
 
 
   -- | - 'ApiAnnotation.AnnKeywordId' : 'ApiAnnotation.AnnCase',
